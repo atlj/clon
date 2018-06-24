6379f30e49eb69ceb8cc2ca25419e318eb71c7c0
atlj <cccswagboyyolo@yahoo.com>
2018-06-25 01:45:08 +0300

    working

--- clon.py
+++ clon.py
@@ -7,37 +7,48 @@ at last can use load function to apply these values to another clon inherited ob
 
 class clon:#don't instance from this, inherit instead
     def dump(self):#use to sum up changed values in one dictionary object
+        returndict = {}
         try:
             self.lastcopy#a dictionary to filter changed values.If there is none, it defines one in order to get catched by a nameerror 
-        except NameError:
-            self.lastcopy = None
-         for var, val in self.__dict__:
+        except AttributeError:
+            self.lastcopy = {}
+        for var in self.__dict__:
             if not var in self.lastcopy:#if a new variable defined since the last dump call, it gets recorded here
-                returndict[var] = val
-
+                returndict[var] = self.__dict__[var]
             else:
-                if not self.lastcopy[var] == val:#if a variable has changed since the last dump call, it gets recorded here 
-                    returndict[var] = val
-
-        self.lastcopy = self.__dict__
+                if not self.lastcopy[var] == self.__dict__[var]:#if a variable has changed since the last dump call, it gets recorded here 
+                    returndict[var] = self.__dict__[var]
+        del self.__dict__["lastcopy"]
+        self.lastcopy = {}
+        for var in self.__dict__:
+            self.lastcopy[var] = self.__dict__[var]
+        if "lastcopy" in returndict:
+            del returndict["lastcopy"]
         return returndict
 
     def dump_all(self):#to be used with compare method
-        return self.__dict__
+        returndict = {}
+        for var in self.__dict__:#copying value
+            returndict[var] = self.__dict__[var]
+
+        if "lastcopy" in returndict:
+            del returndict["lastcopy"]
+        return returndict
 
     def compare(self, data):#to get a dict containing changed or new values
         returndict = {}
-        for var, val in self.__dict__:
+        for var in self.__dict__:
             if not var in data:
-                returndict[var] = val
+                returndict[var] = self.__dict__[var]
 
             else:
-                if not data[var] == val:
+                if not data[var] == self.__dict__[var]:
                     returndict[val] = var
-
+        if "lastcopy" in returndict:
+            del returndict["lastcopy"]
         return returndict
 
 
     def load(self, data):#the main way to load a pre-dumped dict
-        for var, val in data:
-            self.__dict__[var] = val
+        for var in data:
+            self.__dict__[var] = data[var]
